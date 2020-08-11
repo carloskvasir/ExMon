02ada5b1ca3003ea9a0b59442d566a165cd53a01
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2020-08-11 10:51:43 -0300

    feat: salvando state

---
+++ lib/game.ex
@@ -0,0 +1,20 @@
+defmodule ExMon.Game do
+  @doc """
+  %{
+    computer: computer,
+    player: player,
+    turn: :player, #ou :computer
+    status: :started
+  }
+  """
+  use Agent
+
+  def start(computer, player) do
+    initial_value = %{computer: computer, player: player, turn: :player, status: :started}
+    Agent.start_link(fn -> initial_value end, name: __MODULE__)
+  end
+
+  def info do
+    Agent.get(__MODULE__, & &1)
+  end
+end
