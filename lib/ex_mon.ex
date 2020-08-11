db442fa3515b94e87d41cf19d7ca37d1bc7c848d
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2020-08-11 11:17:59 -0300

    feat: Exibindo mensagem de game starded

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,13 +1,18 @@
 defmodule ExMon do
   alias ExMon.{Player, Game}
+  alias ExMon.Game.Status
+
+  @computer_name "Robotinik"
 
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_avg, move_rnd, move_heal)
   end
 
   def start_game(player) do
-    "Robotinik"
+    @computer_name
     |> create_player(:puch, :kick, :heal)
     |> Game.start(player)
+
+    Status.print_round_message()
   end
 end
